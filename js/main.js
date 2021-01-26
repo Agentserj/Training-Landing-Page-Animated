b080043c7f9f678db528ab847fdd175a1b1e59a9
Agentserj <77052473+Agentserj@users.noreply.github.com>
2021-01-26 23:38:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,14 +9,36 @@ btn.onclick = function (e) {
 	document.querySelector('.skills').style.marginLeft = '50px';
 }
 
-// $(function () {
-// 	$(window).scroll(function() {
-// 	    $('#learn .section-title').each(function(){
-// 	        var imagePos = $(this).offset().top;
 
-// 	        var topOfWindow = $(window).scrollTop();
-// 	        if (imagePos < topOfWindow+650) {
-// 	            $(this).addClass("fadeInLeft");
-// 	        }
-// 	    });
-// 	});
+	$(function () {
+		$(window).scroll(function() {
+	    $('#wait .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#ticket .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#ticket .input').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+})
